c47d5f2216ad2ba984298a6601ccafa2f6e78b08
Daylane Silva <111756723+daylaneSilva@users.noreply.github.com>
2025-06-29 12:31:01 -0300

    feat: inicio de submenu

--- src/components/Layout/SideBar/index.jsx
+++ src/components/Layout/SideBar/index.jsx
@@ -1,27 +1,58 @@
 import { NavLink } from "react-router";
 import './index.css'
-import { Home } from "@mui/icons-material";
+import { BuildOutlined, Home, Settings } from "@mui/icons-material";
+import { useState } from "react";
 
 const items = [
-    { name: 'Página Inicial', path: '/home', icon: <Home/>}
+    { name: 'Página Inicial', path: '/home', icon: <Home/>},
+    { name: 'Cadastros', icon: <Settings/>,
+         children: [
+     {name: 'Empresa', icon: <BuildOutlined/>}
+    ]}
 ];
  const Sidebar = ({ abrirSideBar }) => {
+
+    const [openSubmenu, setOpenSubmenu] = useState(null);
+
+    const toggleSubmenu = (name) => {
+    setOpenSubmenu(openSubmenu === name ? null : name);
+    };
+
+
     return(
         <>
          <div>
              <nav className={`sidebar ${abrirSideBar ? "open" : ""}`}>
-                {items.map(({ name, path, icon }) => (
-                  <div className="tooltip-container" key={path}>
-                    <NavLink
+                {items.map(({ name, path, icon, children }) => (
+                  <div className="tooltip-container" key={name}>
+                   {path ? (
+                     <NavLink
                     to={path}
                     className={({ isActive }) => `nav-link ${isActive ? "active" : ""}` }>
                     {icon}
                     {abrirSideBar && <span className="tooltip-text">{name}</span>}
                     </NavLink>
+                   ) : (
+                    <div className="nav-link active" onClick={() => toggleSubmenu(name)}>
+                    {icon}
+                    {abrirSideBar && <span className="tooltip-text">{name}</span>}
+                    </div>
+                   )}
+
+                     {/* Submenu */}
+                    {children && openSubmenu === name && abrirSideBar && (
+                        <div className="submenu">
+                        {children.map((child) => (
+                            <NavLink key={child.path} to={child.path} className="nav-sublink">
+                            <span>{child.name}</span>
+                            </NavLink>
+                        ))}
+                        </div>
+                    )}
                 </div>
                 ))}
-        </nav>
-    </div>
+             </nav>
+        </div>
     </>
     );
 }
